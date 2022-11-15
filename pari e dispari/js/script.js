53d2b2752356414eed1c1c07476a95a79cc1e7dc
Yari <narducciyarigmail.com>
2022-11-15 18:39:14 +0100

    modifiche in js

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,6 +1,6 @@
 //Creiamo una funzione che vada a sommare i numeri pari con i dispari
 
-//Andiamo a creare le noste variabili
+//Andiamo a creare le nostre variabili
 
 let usernum = parseInt(prompt("inserisci un numero da 1 a 5"));
 
@@ -8,7 +8,7 @@ let pcnum = Math.floor(Math.random()* 5) + 1;
 
 
 
-
+// impostiamo un alert se il numero supera il valore 5
 if(usernum > 5){
     alert ("Ti ho detto da 1 a 5 Coglione");
     console.log("Ti ho detto da 1 a 5 Coglione");
@@ -23,5 +23,7 @@ console.log(usernum , pcnum);
 const somma = sommapariDispari(usernum,pcnum);
 
 console.log( "la somma è ",somma);
+
+
 
 
